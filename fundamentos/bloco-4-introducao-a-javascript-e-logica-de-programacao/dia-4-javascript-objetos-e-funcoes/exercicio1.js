0d41b6cf55f0a1c03e982578955c4869e813ffc3
Cleiton <cleitons835@gmail.com>
2021-12-02 09:44:58 -0300

    Inserindo elementos num Objeto

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
@@ -0,0 +1,41 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem vindo, ' + info.personagem);
+
+
+info.recorrente='Sim';
+console.log(info);
+
+
+
+
+
+
+  arrayInformacoes = 
+  [
+    {
+      Data: "Mar 20, 2017 12:00:00 AM", 
+      Atividade: " 23 GERAR", 
+      Observação: "Processo cancelado por: Administrador - as", 
+      Usuário: "afo"
+     }
+  ]
+
+
+data2 = 
+    {
+      "desColigada": "Empresa fulano de tal",
+      "codMatricula": "00555454",
+      "dataImpressao": "23/05/2016"
+      
+    };
+   
+
+    data2.Deucerto =arrayInformacoes;
+
+
+    console.log(data2);
