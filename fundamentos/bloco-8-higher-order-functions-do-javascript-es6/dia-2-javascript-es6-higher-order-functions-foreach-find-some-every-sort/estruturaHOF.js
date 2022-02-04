52deba4edcacc2434205cfec7ec46d8fbcdee4a2
jose-cleiton <cleitons835@gmail.com>
2022-02-04 19:32:36 -0300

    Para fixar

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/estruturaHOF.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/estruturaHOF.js
@@ -38,14 +38,4 @@ const multipliesFor2 = (element) => {
   console.log(`${element} * 2: ${element * 2}`);
 };
 
-numbers.forEach(multipliesFor2);
-
-const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
-
-const convertToUpperCase = (name, index) => {
-  names[index] = name.toUpperCase();
-};
-
-names.forEach(convertToUpperCase);
-console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
-
+numbers.forEach((element) =>  console.log(`${element} * 2: ${element * 2}`))
