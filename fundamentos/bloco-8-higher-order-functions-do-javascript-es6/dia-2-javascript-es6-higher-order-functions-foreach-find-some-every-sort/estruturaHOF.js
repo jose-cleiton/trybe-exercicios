f6b5581427b01c4b9894427d1af74827025f65bf
jose-cleiton <cleitons835@gmail.com>
2022-02-04 19:00:52 -0300

    Exemplo de Foreach3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/estruturaHOF.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/estruturaHOF.js
@@ -31,3 +31,21 @@ arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
 //   Cada elemento do array: { comida: 'Chocolate' }
 //   Index, posição do elemento: 3
 //   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
