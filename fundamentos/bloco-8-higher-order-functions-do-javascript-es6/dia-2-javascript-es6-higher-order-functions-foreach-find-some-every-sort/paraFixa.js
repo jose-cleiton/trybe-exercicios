f3c8054749e93b2da607f322ea8b2f34c087f811
jose-cleiton <cleitons835@gmail.com>
2022-02-06 18:56:53 -0300

    Exibindo no browser

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixa.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixa.js
@@ -2,15 +2,25 @@
  * 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
  */
 
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
+// Array contendo a lista de emails.
+const emailList = [
+  "isa@myemail.com",
+  "lipe@myemail.com",
+  "digo@myemail.com",
+  "greg@myemail.com"
 ];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-};
+// Função callback que recebe o array emailList como parâmetro.
+const showEmailList = (list) => {
+  const div = document.querySelector("#email-list");
+  div.innerHTML = '';
+// Para cada e-mail da lista, a função cria uma tag <p> com a frase '${email}: Enviado com sucesso'
+  list.forEach((email) => {
+    const p = document.createElement("p");
+    p.innerHTML = `${email} : Enviado com sucesso!`;
+    div.appendChild(p);
+  });
+}
 
-emailListInData.forEach(showEmailList)
+const btnFilter = document.querySelector("#btn-filter");
+btnFilter.addEventListener("click", () => showEmailList(emailList));
