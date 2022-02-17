27ebd95a1b736edde6d4cfbf281b4c2de180d0dd
jose-cleiton <cleitons835@gmail.com>
2022-02-17 11:49:35 -0300

    Relógio

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/App.js
@@ -4,13 +4,20 @@ function helloWorld (nome, sobrenome) {
   return <h1 className={classe} >Hello, {`${nome} ${sobrenome}`}</h1>;
 }
 const element = helloWorld("Jorge", "Maravilha");
+
 const container = <div >{element}</div>;
+const time = (
+    <div>
+
+    </div>
+  );
 function App() {
   
 
   return (
     <div className="App">
       {container}
+      {time}
     </div>
   );
 }
--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/index.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Oi, Agora são:</h1>
+      <h2>{new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
