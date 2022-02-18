9580723ec0da1c162a9184c39c472e26082ed352
jose-cleiton <cleitons835@gmail.com>
2022-02-18 13:41:24 -0300

    Colocando exercicios em compomentes.

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exec/src/App.js
@@ -1,13 +1,15 @@
 // src/App.js
 import React from 'react';
-
 import Compras from './components/Compras';
+import ObjAlbum from './components/ObjAlbum';
+
 
 class App extends React.Component {
 render() {
 return (
     <div>   
         <Compras />
+        <ObjAlbum/>
         </div>
     );
     };
