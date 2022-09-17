25b30369b7b2e0838c45a06e866298cce23b8e51
jose-cleiton <cleitons835>
2022-09-17 12:11:27 -0300

    Tratamento de erros

--- back-end/bloco-30-mongodb-com-nodejs-e-poo/guia-mongodb-com-nodejs-e-poo/src/models/MongoModel.ts
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/guia-mongodb-com-nodejs-e-poo/src/models/MongoModel.ts
@@ -1,4 +1,5 @@
 import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
 import IModel from '../interfaces/IModel';
 
 export default abstract class MongoModel<T> implements IModel<T> {
@@ -12,12 +13,12 @@ export default abstract class MongoModel<T> implements IModel<T> {
     return this._model.create({ ...obj });
   }
   public async readOne(_id: string): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
     return this._model.findOne({ _id });
   }
 
   public async update(_id: string, obj: T): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
     return this
       ._model.findByIdAndUpdate(
         _id, 
